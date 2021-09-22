2d3ae2e40e08b6af80a2d363d3ce9fdcc1ce29fd
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-09-22 12:55:46 +0300

    Initial Commit

---
+++ Project18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by othman shahrouri on 9/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
